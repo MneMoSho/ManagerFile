4312c247e429bf2a00256b3b91dc1448263cc437
glebkurylovic@gmail.com <glebkurylovic@gmail.com>
2024-10-02 22:39:43 +0300

    sonar

--- Manager/fileEdit.h
+++ Manager/fileEdit.h
@@ -3,25 +3,25 @@
 class fileEdit
 {
 public:
-    fileEdit(const std::string& firstName, const std::string& fileType, const std::string& information, const int totalLineCount);
-    ~fileEdit();
-    void fileCreate(int numberOfLines, const std::string& name);
-    void fileUpdate(int numberOfLines, std::string name);
-    void fileRead(std::string buf, std::string name);
-    void lineDelete(int lineToDelete, std::string Buf, int currentLine, std::string name);
-    int checkFile() const;
-    std::string getFirstName() const;
-    std::string getFileType() const;
-    void setFirstName(std::string_view name);
-    void setFileType(std::string_view type);
+	fileEdit(const std::string& firstName, const std::string& fileType, const std::string& information, const int totalLineCount);
+	~fileEdit();
+	void fileCreate(int numberOfLines, const std::string& name);
+	void fileUpdate(int numberOfLines, std::string name);
+	void fileRead(std::string buf, std::string name);
+	void lineDelete(int lineToDelete, std::string Buf, int currentLine, std::string name);
+	int checkFile() const;
+	std::string getFirstName() const;
+	std::string getFileType() const;
+	void setFirstName(std::string_view name);
+	void setFileType(std::string_view type);
 
 private:
 
-    std::ifstream fin;
-    std::ofstream fout;
-    int totalLineCount = 0;
-    std::string firstName;
-    std::string fileType;
-    std::string information;
+	std::ifstream fin;
+	std::ofstream fout;
+	int totalLineCount = 0;
+	std::string firstName;
+	std::string fileType;
+	std::string information;
 
 };
