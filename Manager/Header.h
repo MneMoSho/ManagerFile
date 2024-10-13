d32732d45270fcedd57fd3409caef9d653000037
MneMoSho <157227724+MneMoSho@users.noreply.github.com>
2024-10-13 18:09:57 +0300

    Update Header.h

--- Manager/Header.h
+++ Manager/Header.h
@@ -10,7 +10,6 @@
 #include <winbase.h>
 #include <filesystem>
 
-
 void deleteFile(const std::string& directory);
 int linesNumber(int numberOfLines);
 void createNewfile(int numberOfLines, const std::string& destination);
@@ -23,4 +22,6 @@ void addToAnother(const std::string& destination);
 void deleteSimillarTypeFile(const std::string& destination);
 void newDirectory(std::string* destName);
 void newDirectoryFromFile(std::string* destName);
+void chooseDirectory(std::string* destName);
 void appendingPartsOfPath(std::string* name, const std::string& destination);
+void countingDirectories(const std::string& diskName, std::filesystem::path parentPath, int *count);
