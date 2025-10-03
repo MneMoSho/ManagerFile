68c359040ed5a0b72717b6f3ac911e071e0135e8
Kurylovich Hleb <157227724+MneMoSho@users.noreply.github.com>
2025-10-03 21:32:08 +0300

    Add files via upload

---
+++ ListToDelete.h
@@ -0,0 +1,14 @@
+//
+// Created by Gleb on 02.11.2024.
+//
+
+#ifndef UNTITLED4_LISTTODELETE_H
+#define UNTITLED4_LISTTODELETE_H
+
+template <typename T>
+class ListToDelete {
+
+};
+
+
+#endif //UNTITLED4_LISTTODELETE_H
